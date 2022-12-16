7bdc657ee0703fe1a846690109144572b1926f7a
Abhishek Sharma <abhishek3.dev@gmail.com>
2022-12-16 14:05:13 +0530

    Update header

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 const express = require("express");
 const cors = require("cors");
-const ampCors = require('amp-toolbox-cors');
+const ampCors = require("amp-toolbox-cors");
 
 const app = express();
-
 
 app.use(ampCors());
 
 app.get("/", (req, res) => {
 	const { interest } = req.query;
-	res.send({
+	// set headers
+	res.set("Access-Control-Allow-Origin", "*");
+	res.set("Access-Control-Allow-Methods", "GET, POST");
+	res.set("Access-Control-Allow-Headers", "Content-Type");
+	res.set("AMP-Access-Control-Allow-Source-Origin", "https://rtcamp-salesforce-preprod.go-vip.net");
+	res.set("Access-Control-Expose-Headers", "AMP-Access-Control-Allow-Source-Origin");
+	res.set("Content-Type", "application/json");
+	res.set("AMP-Email-Allow-Sender", "*");
+
+	// set response
+	res.json({
 		items: [
 			{
-				interest,
-				template: "<h1>This is heading one.</h1><p>This is a paragraph.</p>",
+				featured_image:
+					"https://rtcamp-salesforce-preprod.go-vip.net/wp-content/uploads/2022/08/799e002f-06b3-39e4-b447-477e915c2151.jpg?w=1348",
+				post_title: "Officia qui repellat aliquid",
+				post_excerpt: "Qui id quia.",
+				categories: ["Arts", "Entertainment", "Events", "Ipsum lorem"],
+				post_link:
+					"https://rtcamp-salesforce-preprod.go-vip.net/blog/2022/08/04/officia-qui-repellat-aliquid/",
+			},
+			{
+				featured_image: "",
+				post_title: "Deleniti numquam sint tenetur aut",
+				post_excerpt:
+					"Assumenda tempore.\n\nEx sequi aspernatur.\n\nCum delectus.",
+				categories: ["Arts", "Ipsum lorem", "News", "Science"],
+				post_link:
+					"https://rtcamp-salesforce-preprod.go-vip.net/blog/2022/08/04/deleniti-numquam-sint-tenetur-aut/",
+			},
+			{
+				featured_image: "",
+				post_title: "Laborum ducimus et debitis dolorem tempore aut",
+				post_excerpt: "Voluptate.",
+				categories: ["Arts", "Events", "Ipsum lorem"],
+				post_link:
+					"https://rtcamp-salesforce-preprod.go-vip.net/blog/2022/08/04/laborum-ducimus-et-debitis-dolorem-tempore-aut/",
 			},
 		],
 	});
